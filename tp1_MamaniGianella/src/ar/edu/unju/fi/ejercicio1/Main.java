5ba276842bc3a35bb487a63c8db485ecb6703711
PC <PC@DESKTOP-QH97CES>
2024-04-15 19:59:15 -0300

    ejercicio 1

---
+++ tp1_MamaniGianella/src/ar/edu/unju/fi/ejercicio1/Main.java
@@ -0,0 +1,17 @@
+package ar.edu.unju.fi.ejercicio1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n1 = 8;
+		int n2 = 7;
+		int n3 = 6;
+		int n4 = 4;
+		int n5 = 6;
+		
+		double promedio = (double)(n1+n2+n3+n4+n5)/5;
+		System.out.println(promedio);
+	}
+
+}
