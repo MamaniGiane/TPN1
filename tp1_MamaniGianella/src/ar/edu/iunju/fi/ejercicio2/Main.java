93d45f577bfff81943e4fb519f16f95f0f28ec6e
PC <PC@DESKTOP-QH97CES>
2024-04-15 21:52:14 -0300

    .

---
+++ tp1_MamaniGianella/src/ar/edu/iunju/fi/ejercicio2/Main.java
@@ -0,0 +1,22 @@
+package ar.edu.iunju.fi.ejercicio2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 String nombrePais = "España";
+	        int edadPersona = 30;
+	        double alturaEdificio = 150.5;
+	        double precioProducto = 1500.99;
+	        String numeroTelefono = "123456789";
+	        double coseno = Math.cos(0.5);
+	        
+	        System.out.println("País: " + nombrePais);
+	        System.out.println("Edad de la persona: " + edadPersona);
+	        System.out.println("Altura del edificio: " + alturaEdificio);
+	        System.out.println("Precio del producto: " + precioProducto);
+	        System.out.println("Número de teléfono: " + numeroTelefono);
+	        System.out.println("Coseno de 0.5: " + coseno);
+	}
+
+}
